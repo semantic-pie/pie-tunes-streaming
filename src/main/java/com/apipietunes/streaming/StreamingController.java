4a720b205bf62fda63965268bc68a15ba6abfcc8
Левков Глеб <levkov.gleb.dev@gmail.com>
2024-03-20 12:14:31 +0300

    fix: api cors bug

--- src/main/java/com/apipietunes/streaming/StreamingController.java
+++ src/main/java/com/apipietunes/streaming/StreamingController.java
@@ -7,6 +7,7 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -22,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class StreamingController {
     @Value("${minio.buckets.tracks}")
